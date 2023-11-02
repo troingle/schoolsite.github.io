078ee5251904cad7ba5447c4af027281e54affe6
troingle <bugsinsidemyhead@gmail.com>
2023-11-02 20:40:42 +0200

    Testing changing BG colour

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// nothing yet lol
+function updateBackgroundColor() {
+    const pos = window.scrollY;
+    const breakpoint = 500; 
+
+    if (pos >= breakpoint) {
+        document.body.style.backgroundColor = 'white';
+    } else {
+        document.body.style.backgroundColor = '#CE5A67';
+    }
+}
+
+window.addEventListener('scroll', updateBackgroundColor);
+
+updateBackgroundColor();
