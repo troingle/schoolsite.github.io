91243e7e58fc6ca577314b5f2d64e9343b5bcc32
troingle <bugsinsidemyhead@gmail.com>
2023-11-02 21:07:01 +0200

    Colour changes, removed nav, removed useless script etc

--- script.js
+++ script.js
@@ -1,18 +1,2 @@
-function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
-function changeBackgroundColor() {
-
-    const color = getRandomColor;
-
-    document.body.style.backgroundColor = color;
-  }
-
-setInterval(changeBackgroundColor, 3000);
+// nothing
 
