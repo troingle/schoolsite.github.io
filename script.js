5db8d6dce72dec072907b526b550ac7235fcda3d
troingle <bugsinsidemyhead@gmail.com>
2023-11-03 05:24:21 +0000

    Made tutor list and finding thingy

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// nothing
+document.addEventListener("DOMContentLoaded", function() {
+    var randomNum = Math.floor(Math.random() * 9) + 2;
+    var randomNumberElement = document.getElementById("randomNumber");
 
+    randomNumberElement.textContent = randomNum;
+});
