4eb612054f53730f988a3f4b0c98a80410c6ab93
troingle <bugsinsidemyhead@gmail.com>
2023-11-03 05:43:45 +0000

    Random tutor generator

--- script.js
+++ script.js
@@ -4,3 +4,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
     randomNumberElement.textContent = randomNum;
 });
+
+const names = [
+    'H. Mäkeläinen',
+    'T. Veijarila',
+    'H. Suo',
+    'S. Niinistö',
+    'M. Kontio',
+    'V. Rantanen',
+    'J. Leskinen'
+]
+
+const imageFiles = ['tutor1.jpeg', 'tutor2.jpeg', 'tutor3.jpeg'];
+
+function getRandomName() {
+    const randomIndex = Math.floor(Math.random() * names.length);
+    return names[randomIndex];
+}
+
+function getRandomImage() {
+    const randomIndex = Math.floor(Math.random() * imageFiles.length);
+    return imageFiles[randomIndex];
+}
+
+function updatePage() {
+    const randomName = getRandomName();
+    const randomImage = getRandomImage();
+
+    document.getElementById('name').textContent = randomName;
+    document.getElementById('image').src = randomImage;
+}
+
+updatePage();
